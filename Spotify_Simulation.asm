c699a9e5ae337bc2e91c9f2fdd98c6884c02acf9
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-06-02 16:45:37 -0600

    Cambios hechos con buffer

--- Spotify_Simulation.asm
+++ Spotify_Simulation.asm
@@ -11,6 +11,7 @@
 ;Fecha de entrega: -/05/2023
 
 
+
 .386 ; Corregido
 .model flat, stdcall, c ; Corregido
 .stack 4096
@@ -25,6 +26,7 @@
 	msgMenu3 BYTE "2. Lista de reproduccion 'Para pasar el rato'", 0Ah,0
 	strOP BYTE 255 DUP(?);
 	strOP2 BYTE 255 DUP(?);
+	strDatosCancion BYTE 255 DUP(?);
 
 
 	comparador word ?
@@ -39,15 +41,33 @@
 	msgAgregarCancion BYTE "Ingrese el nombre de la canción: ", 0
 	msgNoHayCanciones BYTE "No hay canciones en la lista.", 0
 
+	msgIngresoCancion BYTE "Ingresa el Nombre del Artista: | Nombre Canción | Duración Total | Letra.", 0
 
 
 
-	arr BYTE "Nombre Artista: Bad Bunny y Grupo Frontera | Nombre Canción: un x100to | Duracion Total: 3:14 min","Nombre Artista: Lil Durk | Nombre Canción: All My Life| Duracion Total: 4:04 min","Nombre Artista: Manuel Turizo y Marshmello | Nombre Canción: El merengue | Duracion Total: 2 min","Nombre Artista: Alan Walker | Nombre Canción: Alone | Duración Total: 2:44 min","Nombre Artista: Kendrick Lamar ft. SZA | Nombre Canción: All the Stars | Duración Total: 3:52 min"
+
+
+	arr BYTE "Nombre Artista: Bad Bunny y Grupo Frontera | Nombre Canción: un x100to | Duracion Total: 3:14 min","Nombre Artista: Lil Durk | Nombre Canción: All My Life| Duracion Total: 4:04 min","Nombre Artista: Manuel Turizo y Marshmello | Nombre Canción: El merengue | Duracion Total: 2 min","Nombre Artista: Alan Walker | Nombre Canción: Alone | Duración Total: 2:44 min","Nombre Artista: Kendrick Lamar ft. SZA | Nombre Canción: All the Stars | Duración Total: 3:52 min"," "," "," "," "
+
+
+
 
 	longitud equ ($ - arr) / ($ - arr[0])                     ; Número de canciones en el array
 	contador dword 4	
 
 	texto BYTE "hola soy nuevo" 
+
+	strBuff1 BYTE "Nombre Artista: Bad Bunny y Grupo Frontera | Nombre Canción: un x100to | Duracion Total: 3:14 min "
+	strBuff2 BYTE "Nombre Artista: Lil Durk | Nombre Canción: All My Life| Duracion Total: 4:04 min"
+	strBuff3 BYTE "Nombre Artista: Manuel Turizo y Marshmello | Nombre Canción: El merengue | Duracion Total:4MIN"
+	strBuff4 BYTE "Nombre Artista: Alan Walker | Nombre Canción: Alone | Duración Total: 2:44 min"
+	strBuff5 BYTE "Nombre Artista: Kendrick Lamar ft. SZA | Nombre Canción: All the Stars | Duración Total: 3 miN"
+	strBuff6 BYTE 255 DUP(?)
+	strBuff7 BYTE 255 DUP(?)
+
+	resultMsg BYTE 'La nueva canción es: %s', 0Ah, 0
+	resultMsg1 BYTE 'Se elimino con exito la cancion: %s', 0Ah, 0
+
 
 .code
 	includelib libucrt.lib
@@ -123,19 +143,27 @@ opcion1:
 
 	mov cx, '1' ; Corregido
 	mov dx, '2' ; Corregido
-	;mov ex, '3' ; Corregido
+	mov ax, '3' ; Corregido
 
-	cmp cx, word ptr strOP ; Comparar ax con strOP
+	cmp cx, word ptr strOP2 ; Comparar cx con strOP2
 	je opcion1Menu2 ; Saltar si son iguales
 
-	cmp dx, word ptr strOP ; Comparar ax con strOP
-	je opcion2Menu2 ; Saltar si son iguales
+	cmp dx, word ptr strOP2 ; Comparar dx con strOP2
+	je opcion1Menu2Agregar ; Saltar si son iguales
+
+	cmp ax, word ptr strOP2 ; Comparar dx con strOP2
+	je opcion1Menu2Eliminar ; Saltar si son iguales
 
 	;jmp opcion1
 	jmp fin_menu
 
 opcion1Menu2:
 	 ; Recorrer el array de canciones
+
+	push offset strBuff6 	; Empujar formato a la pila
+	push offset resultMsg 	; Empujar formato a la pila
+    call printf 
+
     mov esi, offset arr      ; Cargar la dirección base del array en ESI
     
     mov ecx, longitud        ; Cargar el número de canciones en ECX
@@ -159,87 +187,40 @@ opcion1Menu2:
     
     ; Fin del recorrido
     ; ...
-	jmp fin_menu
 
+	jmp opcion1
+
+
+opcion1Menu2Agregar:
 ;opcion1Menu2Agregar:
-    ; Agregar canción
-    ;push offset msgAgregarCancion ; Mensaje para solicitar el nombre de la canción
-    ;call printf
-
-    ; Obtener el nombre de la canción ingresado por el usuario
-    ;lea eax, texto
-    ;push eax
-    ;push offset fmt
-    ;call scanf
-
-    ; Agregar la canción al array arr
-    ;mov esi, offset arr      ; Cargar la dirección base del array en ESI
-    ;mov eax, contador        ; Obtener el contador actual
-    ;imul eax, 8              ; Multiplicar el contador por 8 (cada canción ocupa 8 bytes)
-    ;add eax, esi             ; Calcular la dirección de la última posición del array
-    ;mov edi, offset texto    ; Obtener la dirección del nombre de la canción ingresado por el usuario
-    ;mov ecx, 255             ; Longitud máxima del nombre de la canción
-    ;rep movsb                ; Copiar el nombre de la canción al array arr
-
-    ; Actualizar el contador y la longitud del array
-    ;inc contador
-    ;inc longitud
-
-    ;jmp fin_menu
-
-;opcion1Menu2Eliminar:
-    ; Eliminar última canción
-    ;cmp contador, 0           ; Verificar si hay canciones en la lista
-    ;je noHayCanciones         ; Saltar si no hay canciones
-
-    ;dec contador              ; Decrementar el contador para indicar que se elimina una canción
-    ;mov esi, offset arr       ; Cargar la dirección base del array en ESI
-    ;mov eax, contador         ; Obtener el contador actual
-    ;imul eax, 8               ; Multiplicar el contador por 8 (cada canción ocupa 8 bytes)
-    ;add eax, esi              ; Calcular la dirección de la última canción en el array
-    ;mov ecx, 8                ; Tamaño de una canción en bytes (nombre del artista + nombre de la canción + duración)
-    ;mov edi, eax              ; Guardar la dirección de la última canción en EDI
-    ;xor al, al                ; Llenar con ceros para eliminar la canción
-    ;rep stosb                 ; Eliminar la canción
-
-    ;jmp fin_menu
-
-;noHayCanciones:
-    ; Mostrar mensaje de que no hay canciones en la lista
-    ;push offset msgNoHayCanciones
-    ;call printf
-    ;jmp fin_menu
-
-
-opcion2Menu2:
-
-
 	
-	;mov arr [contador], texto
-	jmp fin_menu
-
-opcion3Menu2:
-	jmp fin_menu
-
-opcion2:
-	push offset msgM2ms2 ; Corregido
+    ; Agregar canción
+	push offset msgIngresoCancion
 	call printf
 
-	push offset msgMenu3A
-	call printf
-	push offset msgMenu3B
-	call printf
-	push offset msgMenu3C
-	call printf
-	push offset msgMenu3D
-	call printf
 
-	jmp fin_menu
+	;obtener dato de canción 
+	;push ebp
+	;mov ebp, esp
+
+    lea  eax, strBuff6 		; Obtener dirección del buffer
+    push eax 				; Empujar dirección a la pila
+    push offset fmt 		; Empujar formato a la pila
+    call scanf 				; Leer cadena desde la entrada estándar
+
+    push offset strBuff6 	; Empujar cadena a la pila
+    push offset resultMsg 	; Empujar formato a la pila
+    call printf 			; Imprimir resultado
+
+    add esp, 8 				; Limpiar la pila
+
+    mov esp, ebp
+    pop ebp
+    
+	push 0
+    jmp opcion1
 
 
-fin_menu:
-	RET
 
-menu endp
 
-end
+    ;jmp fin_menu
